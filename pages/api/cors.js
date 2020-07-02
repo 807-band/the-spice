6a9ef339b03bff90ed96b7add111412725b3ebe8
bcollard <bcollard@callfire.com>
2020-07-01 20:07:54 -0700

    now connects to localhost:3001 as server

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
    // Run cors
    await cors(req, res)
    
-   const stations = await axios.get('http://ec2-52-42-80-173.us-west-2.compute.amazonaws.com:3001/api/station')
+   const stations = await axios.get('http://localhost:3001/api/station')
    
    // Rest of the API logic
    // res.json({ message: 'Hello Everyone!' })
