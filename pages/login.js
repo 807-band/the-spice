b5d4523c949e44ae13032c256fad03dca993289e
bcollard <bcollard@callfire.com>
2020-09-06 13:02:14 -0700

    cleaning up user auth

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,27 @@
 import { Form, Button } from 'react-bootstrap'
-import { login, getUser } from '../lib/users'
-import { parseCookies } from 'nookies'
+import { login } from '../lib/users'
 import { withRouter } from 'next/router'
 
 class Login extends React.Component {
    constructor(props) {
       super(props);
+
+      this.state = {
+         incorrectUsername: false,
+         incorrectPassword: false
+      }
    }
 
    handleSubmit = async (event) => {
       event.preventDefault();
-      await login(event.currentTarget.username.value, event.currentTarget.password.value);
+      const res = await login(event.currentTarget.username.value, event.currentTarget.password.value);
+      if(res == 'success')
+         this.props.router.push('/');
+      else if(res == 'incorrectUsername')
+         this.setState({ incorrectUsername: true, incorrectPassword: true });
+      else if(res == 'incorrectPassword')
+         this.setState({ incorrectUsername: false, incorrectPassword: true });
+      
    }
 
    componentDidMount() {
@@ -26,12 +37,12 @@ class Login extends React.Component {
             <Form onSubmit={this.handleSubmit}>
                <Form.Group controlId="username">
                   <Form.Label>Username</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control isInvalid={this.state.incorrectUsername} type="text" />
                </Form.Group>
 
                <Form.Group controlId="password">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" />
+                  <Form.Control isInvalid={this.state.incorrectPassword} type="password" />
                </Form.Group>
 
                <Button variant="primary" type="submit">
@@ -42,15 +53,5 @@ class Login extends React.Component {
       )
    }
 }
-
-// export async function getServerSideProps(ctx) {
-//    const cookies = parseCookies(ctx);
-//    const currentUser = await getUser(cookies.currUserID);
-//    return {
-//       props: {
-//          currentUser
-//       }
-//    }
-// }
 
 export default withRouter(Login);
