936e05fd6a3278945f0d923ddb9a226df3ea516f
Ethan Zimbelman <itszimbo@gmail.com>
2020-09-07 13:53:49 -0700

    multiple info card functionality

--- components/StationInfo.js
+++ components/StationInfo.js
@@ -1,5 +1,5 @@
 import { Card, Button, Form } from 'react-bootstrap'
-import { putInformation } from '../lib/stations'
+import { putInformation, createInfoTab } from '../lib/stations'
 import Link from 'next/link'
 
 export default class StationInfo extends React.Component {
@@ -8,12 +8,23 @@ export default class StationInfo extends React.Component {
 
       this.state = {
          editing: false,
-         text: props.pageData.content
+         content: props.pageData
       }
    }
 
    render() {
-      if (!this.state.editing)
+      if (!this.state.editing) {
+        const cards = this.state.content.map(c => {
+          return (
+            <Card key={c.packetID}>
+              <Card.Header className="card-header"></Card.Header>
+              <Card.Body>
+                <Card.Text className="multiline-text">{c.content}</Card.Text>
+              </Card.Body>
+            </Card>
+          )
+        });
+
          return (
             <>
                <Button className="edit-button" onClick={() => this.setState({ editing: true })}>
@@ -25,41 +36,60 @@ export default class StationInfo extends React.Component {
                   </Button>
                </Link>
                <br />
-               <Card>
-                  <Card.Header className="card-header">{this.props.pageData.role} {this.props.pageData.info}</Card.Header>
-                  <Card.Body>
-                     <Card.Text className="multiline-text">
-                       {this.state.text}
-                     </Card.Text>
-                  </Card.Body>
-               </Card>
+                { cards }
             </>
          );
-      return (
-         <Form onSubmit={this.submitForm}>
-            <Button variant="primary" type="submit" className="edit-button">
-               Save
-            </Button>
-            <Button variant="secondary" className="edit-button" onClick={() => this.setState({ editing: false })}>
-               Cancel
-            </Button>
-            <br />
-            <Card>
-               <Card.Header className="card-header">{this.props.pageData.role} {this.props.pageData.info}</Card.Header>
-               <Card.Body>
-                  <Card.Text>
-                     <Form.Control id="text" defaultValue={this.state.text} as="textarea" rows="30"/>
-                  </Card.Text>
-               </Card.Body>
-            </Card>
+      }
 
-         </Form>
+      const cards = this.state.content.map(c => this.createEditCard(c));
+
+      return (
+        <>
+          <Button variant="secondary" className="edit-button" onClick={() => window.location.reload(false)}>
+             Done Editing
+          </Button>
+          <br />
+          <div id="info-cards">{ cards }</div>
+          <br />
+          <Button onClick={this.addCard}>Add Card</Button>
+        </>
       );
    }
 
-   submitForm = (event) => {
-      event.preventDefault();
-      putInformation(this.props.id, this.props.pageData.packetID, event.currentTarget.text.value);
-      this.setState({editing: false, text: event.currentTarget.text.value});
-   }
+  saveInfo = (event) => {
+    event.preventDefault();
+    const infoID = event.target.getAttribute('info-key');
+    const stationID = this.props.id;
+    const content = event.currentTarget.text.value;
+    putInformation(stationID, infoID, content);
+  }
+
+  addCard = async () => {
+    const stationID = this.props.id;
+    // relies on having at least one on the page
+    const role = this.props.pageData[0].role;
+    const info = this.props.pageData[0].info;
+
+    const cardInfo = await createInfoTab(stationID, role, info);
+    const newCard = this.createEditCard(cardInfo);
+    this.setState({content: [...this.state.content, newCard]});
+    // will not add a complete new form
+    // reloading will make it so the new box works :/
+    window.location.reload(false)
+  }
+
+  createEditCard = (c) => {
+    return (
+      <Form onSubmit={this.saveInfo} info-key={c.packetID}>
+        <Card>
+          <Card.Header className="card-header"></Card.Header>
+          <Card.Body>
+            <Card.Text className="multiline-text">
+              <Form.Control id="text" defaultValue={c.content} as="textarea" rows="20" maxlength="4000" />
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Button type="submit">Save</Button>
+      </Form>
+    )};
 }
--- pages/stations/[id]/instructor/setup.js
+++ pages/stations/[id]/instructor/setup.js
@@ -2,7 +2,7 @@ import StationInfo from '../../../../components/StationInfo'
 import { getInformation } from '../../../../lib/stations'
 
 export default function Setup({information, id}) {
-   const pageData = information.find(element => element.role=="instructor" && element.info=="setup");
+   const pageData = information.filter(element => element.role=="instructor" && element.info=="setup");
    return (
       <StationInfo id={id} pageData={pageData}/>
    )
